170d9530570538e8f1669faa448bce2113b74c77
Luan <santos.luan.pereira@gmail.com>
2016-08-22 23:12:26 -0300

    Adicação de algumas coisas da aula 22-08

---
+++ assets/javascript/principal.js
@@ -0,0 +1,13 @@
+//Login
+var formLogin = document.getElementById("formLogin");
+var emailLogin = document.getElementById("emailLogin");
+var senhaLogin = document.getElementById("senhaLogin");
+
+//Busca
+var formBusca = document.getElementById("formBusca");
+var estadoPartida = document.getElementById("selectEstadoPartida");
+var cidadePartida = document.getElementById("selectCidadePartida");
+var nomeEscola = document.getElementById("inputEscola");
+var estadoChegada = document.getElementById("selectCidadeChegada");
+var cidadeChegada = document.getElementById("selectCidadeChegada");
+
