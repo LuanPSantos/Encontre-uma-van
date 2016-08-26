9e636dd9bc7c004158265d3ee1d12e04f71b3eb8
Luan <santos.luan.pereira@gmail.com>
2016-08-26 17:44:17 -0300

    select funcionando - index

--- assets/javascript/automatizacao.js
+++ assets/javascript/automatizacao.js
@@ -74,4 +74,47 @@ $(document).ready(function (){
     $(".pNomeEmpresas").on("click", function(){
         $(this).next().slideToggle();
     });
+
+    // Adicionar cidades e estados nos selects
+    var estados_cidades = {};
+    var cidadespartida = document.getElementById("selectCidadePartida");
+    var cidadesChegada = document.getElementById("selectCidadeChegada");
+    var selectEstadoPartida = document.getElementById("selectEstadoPartida");
+    var selectEstadoChegada = document.getElementById("selectEstadoChegada");
+    firebase.database().ref('/estados-consulta').once('value').then(function(snapshot){
+        estados_cidades = snapshot.val();
+
+        for(var i = 0; i < estados_cidades.length; i++){
+            $("#selectEstadoPartida").append('<option>'+ estados_cidades[i].sigla +'</option>');
+            $("#selectEstadoChegada").append('<option>'+ estados_cidades[i].sigla +'</option>');
+        }
+    });
+
+    $("#selectEstadoPartida").on("change",function(){
+        
+        for(var i = 0; i < estados_cidades.length; i++){
+            if(estados_cidades[i].sigla == $("#selectEstadoPartida :selected").text()){
+                $("#selectCidadePartida").empty();
+                for(var j = 0; j < estados_cidades[i].cidades.length; j++){                    
+                    $("#selectCidadePartida").append('<option>'+ estados_cidades[i].cidades[j] +'</option>');                    
+                }
+            }
+        }
+
+    });
+
+    $("#selectEstadoChegada").on("change",function(){
+        
+        for(var i = 0; i < estados_cidades.length; i++){
+            if(estados_cidades[i].sigla == $("#selectEstadoChegada :selected").text()){
+                $("#selectCidadeChegada").empty();
+                for(var j = 0; j < estados_cidades[i].cidades.length; j++){                                        
+                    $("#selectCidadeChegada").append('<option>'+ estados_cidades[i].cidades[j] +'</option>');
+                }
+            }
+        }
+
+    });
+
+    
 });
